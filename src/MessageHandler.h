4a87e1df9761e7117bed741b18594a518d4c2af8
MiddleRaster <MiddleRaster@users.noreply.github.com>
2026-03-15 16:31:51 -0400

    Refactored heavily to make Win32 app look like C++

---
+++ src/MessageHandler.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <windows.h>
+
+class MessageHandler
+{
+    HWND edit{};
+
+    void SaveEditTextToFile(HWND hWnd, const wchar_t* path)
+    {
+        const LRESULT length = SendMessageW(edit, WM_GETTEXTLENGTH, 0, 0);
+        std::wstring text(static_cast<size_t>(length) + 1, L'\0');
+        SendMessageW(edit, WM_GETTEXT, static_cast<WPARAM>(text.size()), reinterpret_cast<LPARAM>(text.data()));
+        text.resize(static_cast<size_t>(length));
+
+        const int utf8Size = WideCharToMultiByte(CP_UTF8, 0, text.data(), static_cast<int>(text.size()), nullptr, 0, nullptr, nullptr);
+        if (utf8Size < 0)
+            return;
+
+        std::vector<char> utf8(static_cast<size_t>(utf8Size) + 3);
+        utf8[0] = static_cast<char>(0xEF);
+        utf8[1] = static_cast<char>(0xBB);
+        utf8[2] = static_cast<char>(0xBF);
+        WideCharToMultiByte(CP_UTF8, 0, text.data(), static_cast<int>(text.size()), utf8.data() + 3, utf8Size, nullptr, nullptr);
+
+        HANDLE file = CreateFileW(path, GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+        if (file == INVALID_HANDLE_VALUE)
+            return; // TODO: BUGBUG: report error to user
+
+        DWORD written = 0;
+        WriteFile(file, utf8.data(), static_cast<DWORD>(utf8.size()), &written, nullptr);
+        CloseHandle(file);
+    }
+
+public:
+    static MessageHandler& GetHandler(HWND hWnd) { return *reinterpret_cast<MessageHandler*>(GetWindowLongPtrW(hWnd, GWLP_USERDATA)); }
+
+    void Handle_WM_CREATE(HWND hWnd)
+    {
+        edit = CreateWindowExW(WS_EX_CLIENTEDGE, L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_VSCROLL | WS_HSCROLL | ES_LEFT | ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL, 0, 0, 0, 0, hWnd, nullptr, GetModuleHandle(nullptr), nullptr);
+        SetWindowLongPtrW(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this));
+    }
+    void Handle_WM_NCDESTROY(HWND hWnd)
+    {
+        SetWindowLongPtrW(hWnd, GWLP_USERDATA, 0);
+        delete this;
+    }
+
+    void Handle_WM_SIZE(int width, int height) { MoveWindow(edit, 0, 0, width, height, TRUE); }
+
+    void Handle_SaveAs(HWND hWnd)
+    {
+        wchar_t filePath[MAX_PATH] = L"";
+        OPENFILENAMEW ofn{};
+        ofn.lStructSize  = sizeof(ofn);
+        ofn.hwndOwner    = hWnd;
+        ofn.lpstrFile    = filePath;
+        ofn.nMaxFile     = MAX_PATH;
+        ofn.lpstrFilter  = L"Text Files (*.txt)\0*.txt\0All Files (*.*)\0*.*\0";
+        ofn.nFilterIndex = 1;
+        ofn.Flags        = OFN_OVERWRITEPROMPT | OFN_PATHMUSTEXIST;
+
+        if (GetSaveFileNameW(&ofn))
+        {
+            SaveEditTextToFile(hWnd, filePath);
+            std::wstring title = std::filesystem::path{ filePath }.filename().wstring();
+            if (!title.empty())
+                SetWindowTextW(hWnd, title.c_str());
+        }
+    }
+};
--- src/winmain.cpp
+++ src/winmain.cpp
@@ -3,71 +3,10 @@
 #include <filesystem>
 #include <string>
 #include <vector>
+#include "MessageHandler.h"
 #include "Resource.h"
 
 #define MAX_LOADSTRING 100
-
-static void SaveEditTextToFile(HWND hWnd, const wchar_t* path)
-{
-    HWND edit = reinterpret_cast<HWND>(GetWindowLongPtrW(hWnd, GWLP_USERDATA)); // GetEditHandle(hWnd);
-    if (edit == nullptr)
-        return;
-
-    const LRESULT length = SendMessageW(edit, WM_GETTEXTLENGTH, 0, 0);
-    std::wstring text(static_cast<size_t>(length) + 1, L'\0');
-    SendMessageW(edit, WM_GETTEXT, static_cast<WPARAM>(text.size()), reinterpret_cast<LPARAM>(text.data()));
-    text.resize(static_cast<size_t>(length));
-
-    const int utf8Size = WideCharToMultiByte(CP_UTF8, 0, text.data(), static_cast<int>(text.size()), nullptr, 0, nullptr, nullptr);
-    if (utf8Size < 0)
-        return;
-
-    std::vector<char> utf8(static_cast<size_t>(utf8Size) + 3);
-    utf8[0] = static_cast<char>(0xEF);
-    utf8[1] = static_cast<char>(0xBB);
-    utf8[2] = static_cast<char>(0xBF);
-    WideCharToMultiByte(CP_UTF8, 0, text.data(), static_cast<int>(text.size()), utf8.data() + 3, utf8Size, nullptr, nullptr);
-
-    HANDLE file = CreateFileW(path, GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
-    if (file == INVALID_HANDLE_VALUE)
-        return;
-
-    DWORD written = 0;
-    WriteFile(file, utf8.data(), static_cast<DWORD>(utf8.size()), &written, nullptr);
-    CloseHandle(file);
-}
-
-static void DoSaveAs(HWND hWnd)
-{
-    wchar_t filePath[MAX_PATH] = L"";
-    OPENFILENAMEW ofn{};
-    ofn.lStructSize  = sizeof(ofn);
-    ofn.hwndOwner    = hWnd;
-    ofn.lpstrFile    = filePath;
-    ofn.nMaxFile     = MAX_PATH;
-    ofn.lpstrFilter  = L"Text Files (*.txt)\0*.txt\0All Files (*.*)\0*.*\0";
-    ofn.nFilterIndex = 1;
-    ofn.Flags        = OFN_OVERWRITEPROMPT | OFN_PATHMUSTEXIST;
-
-    if (GetSaveFileNameW(&ofn))
-    {
-        SaveEditTextToFile(hWnd, filePath);
-        std::wstring title = std::filesystem::path{filePath}.filename().wstring();
-        if (!title.empty())
-            SetWindowTextW(hWnd, title.c_str());
-    }
-}
-
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow, const WCHAR* szWindowClass, const WCHAR* szTitle)
-{
-    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-    if (!hWnd)
-        return FALSE;
-
-    ShowWindow(hWnd, nCmdShow);
-    UpdateWindow(hWnd);
-    return TRUE;
-}
 
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -90,27 +29,17 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+
     switch (message)
     {
-    case WM_CREATE:
-        {
-            HWND edit = CreateWindowExW(WS_EX_CLIENTEDGE, L"EDIT", L"",WS_CHILD|WS_VISIBLE|WS_VSCROLL|WS_HSCROLL|ES_LEFT|ES_MULTILINE|ES_AUTOVSCROLL|ES_AUTOHSCROLL, 0,0,0,0, hWnd, nullptr, GetModuleHandle(nullptr), nullptr);
-            SetWindowLongPtrW(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(edit));
-        }
-        break;
-    case WM_SIZE:
-        {
-            HWND edit = reinterpret_cast<HWND>(GetWindowLongPtrW(hWnd, GWLP_USERDATA));
-            if (edit != nullptr)
-                MoveWindow(edit, 0, 0, LOWORD(lParam), HIWORD(lParam), TRUE);
-        }
-        break;
+    case WM_CREATE:              (new MessageHandler())->Handle_WM_CREATE(hWnd);    break;
+    case WM_NCDESTROY:  MessageHandler::GetHandler(hWnd).Handle_WM_NCDESTROY(hWnd); break;
+    case WM_SIZE:       MessageHandler::GetHandler(hWnd).Handle_WM_SIZE(LOWORD(lParam), HIWORD(lParam));    break;
     case WM_COMMAND:
-        // Parse the menu selections:
         switch (LOWORD(wParam))
-        {
+        {   // Parse the menu selections:
         case IDM_SAVEAS:
-            DoSaveAs(hWnd);
+            MessageHandler::GetHandler(hWnd).Handle_SaveAs(hWnd);
             break;
         case IDM_ABOUT:
             DialogBox(GetModuleHandle(nullptr), MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
@@ -122,14 +51,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             return DefWindowProc(hWnd, message, wParam, lParam);
         }
         break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Add any drawing code that uses hdc here...
-            EndPaint(hWnd, &ps);
-        }
-        break;
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
@@ -139,8 +60,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
-ATOM MyRegisterClass(HINSTANCE hInstance, const WCHAR* szWindowClass)
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE /*hPrevInstance*/, _In_ LPWSTR /*lpCmdLine*/, _In_ int nCmdShow)
 {
+    WCHAR szTitle      [MAX_LOADSTRING]; // The title bar text
+    WCHAR szWindowClass[MAX_LOADSTRING]; // the main window class name
+    LoadStringW(hInstance, IDS_APP_TITLE_NOTEPAD, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_NOTEPAD,     szWindowClass, MAX_LOADSTRING);
+
     WNDCLASSEXW wcex;
     wcex.cbSize        = sizeof(WNDCLASSEX);
     wcex.style         = CS_HREDRAW | CS_VREDRAW;
@@ -154,23 +80,13 @@ ATOM MyRegisterClass(HINSTANCE hInstance, const WCHAR* szWindowClass)
     wcex.lpszMenuName  = MAKEINTRESOURCEW(IDC_NOTEPAD);
     wcex.lpszClassName = szWindowClass;
     wcex.hIconSm       = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL_NOTEPAD));
-    return RegisterClassExW(&wcex);
-}
+    RegisterClassExW(&wcex);
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
-{
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
-
-    WCHAR szTitle      [MAX_LOADSTRING]; // The title bar text
-    WCHAR szWindowClass[MAX_LOADSTRING]; // the main window class name
-
-    // Initialize global strings
-    LoadStringW(hInstance, IDS_APP_TITLE_NOTEPAD, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_NOTEPAD,     szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance, szWindowClass);
-    if (!InitInstance (hInstance, nCmdShow, szWindowClass, szTitle))
-        return FALSE;
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+    if (!hWnd)
+        return -1;
+    ShowWindow  (hWnd, nCmdShow);
+    UpdateWindow(hWnd);
 
     HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_NOTEPAD));
     MSG msg;
